3de76b2523d415fa5cf6a75bc6ab1e91b02a9577
Duane Goodner <dmgoodner@gmail.com>
2020-10-30 19:12:42 +0800

    Add launch_foreground function. Compiles but not tested.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <signal.h>
+#include <fcntl.h>
 #include "command.h"
 #include "utilities.h"
 
@@ -18,33 +20,27 @@
 #define LAST_FG_TERMINATED "The last foreground process was terminated by signal"
 #define LAST_FG_EXITED "The last foreground process terminated normally with exit status"
 
+// TO DO: add fflush() after every print statement
 
 struct bgpid_node
 {
     pid_t process_id;
     struct bgpid_node *next;
 };
-
 struct bgpid_node *bg_list_head = NULL;
 struct bgpid_node *bg_list_tail = NULL;
 //struct bgpid_node* create_bg_pidnode(pid_t process_id);
 
 int last_fg_endsig = 0;
 char* last_fg_endmsg = NO_FG_RUN_YET;
+bool last_fg_terminated = false;
+
 bool bg_launch_allowed = true;
-bool any_zombies = false;
+bool potential_zombies = false;
 
-//int execute_command(struct command* curr_command);
-// int launch_foreground(struct command* curr_command);
-int launch_background(struct command* curr_command);
-int launch_foreground(struct command* curr_commane);
-
-
-int exit_bltin(struct command* curr_command);
-int cd_bltin(struct command* curr_command);
-int status_bltin(struct command* curr_command);
-int get_num_bltins(void);
-int get_bltin_index(struct command* curr_command);
+int cd_bltin(struct command* cd_command);
+int status_bltin(struct command* status_command);
+int exit_bltin(struct command* exit_command);
 
 char *bltin_funct_names[] = {
     "cd",
@@ -81,22 +77,6 @@ void killall_bgprocs(struct bgpid_node* bgpid_list) {
     }
 }
 
-int exit_bltin(struct command* exit_command) {
-
-    killall_bgprocs(bg_list_head);
-
-    return 0;
-
-}
-
-int status_bltin(struct command* status_command) {
-    
-    printf("%s %d\n", last_fg_endmsg, last_fg_endsig);
-
-    return 1;
-
-}
-
 int cd_bltin(struct command* cd_command) {
     
     int chdir_return;
@@ -114,9 +94,27 @@ int cd_bltin(struct command* cd_command) {
     return 1;
 }
 
+int status_bltin(struct command* status_command) {
+    
+    printf("%s %d\n", last_fg_endmsg, last_fg_endsig);
+
+    return 1;
+
+}
+
+int exit_bltin(struct command* exit_command) {
+
+    killall_bgprocs(bg_list_head);
+
+    return 0;
+}
+
+
 #define ENTER_FG_ONLY_MSG "Entering foreground-only mode (& is now ignored)\n"
 #define EXIT_FG_ONLY_MSG "Exiting foreground-only mode\n"
 void handle_SIGTSTP (int signo) {
+    //TO DO: modify so handler only changes bool, then use separate function
+    //to output messages at start of main while loop.
     bg_launch_allowed = !bg_launch_allowed;
     if (!bg_launch_allowed) {
         write(STDOUT_FILENO, ENTER_FG_ONLY_MSG, 49);
@@ -126,17 +124,115 @@ void handle_SIGTSTP (int signo) {
     write(STDOUT_FILENO, C_PROMPT, 2);       
 }
 
+void handle_SIGCHLD (int signo) {
+    potential_zombies = true;
+}
+
 void set_shell_sighandlers() {
     struct sigaction ignore_action = {0};
-    struct sigaction SIGTSTP_action = {0};
-
     ignore_action.sa_handler = SIG_IGN;
     sigaction(SIGINT, &ignore_action, NULL);
 
+    struct sigaction SIGTSTP_action = {0};
     SIGTSTP_action.sa_handler = handle_SIGTSTP;
     SIGTSTP_action.sa_flags = SA_RESTART;
     sigaction(SIGTSTP, &SIGTSTP_action, NULL);
+
+    struct sigaction SIGCHLD_action = {0};
+    SIGCHLD_action.sa_handler = handle_SIGCHLD;
+    SIGCHLD_action.sa_flags = SA_RESTART;
+    sigaction(SIGCHLD, &SIGCHLD_action, NULL);
 }
+
+void set_fgchild_sighandlers() {
+    struct sigaction SIGINT_action = {0};
+    SIGINT_action.sa_handler = SIG_DFL;
+    SIGINT_action.sa_flags = SA_RESTART; // Is SA_RESTART flag necessary???
+    sigaction(SIGINT, &SIGINT_action, NULL);
+}
+
+void redirect_ouptut(char* new_out_path) {
+    int out_fd = open(new_out_path, O_WRONLY | O_CREAT | O_TRUNC, 640);
+    if (out_fd == -1) {
+        fprintf(stderr, "Failed open %s for output", new_out_path);
+        exit(1);
+    }
+
+    int dup2_result = dup2(out_fd, 1);
+    if (dup2_result == -1) {
+        perror("output dup2 error");
+        exit(2);
+    }
+}
+
+void redirect_input(char* new_in_path) {
+    int in_fd = open(new_in_path, O_RDONLY);
+    if (in_fd == -1) {
+        fprintf(stderr, "cannot open %s for input", new_in_path);
+        exit(1);
+    }
+
+    int dup2_result = dup2(in_fd, 0);
+    if (dup2_result == -1) {
+        fprintf(stderr, "input dup2 error");
+    }
+}
+
+int launch_foreground(struct command* curr_command) {
+
+    int fgchild_status;
+    pid_t fgchild_pid = fork();
+
+    if (fgchild_pid == -1) {
+        perror("fork() failed.");
+        exit(1);
+    
+    // child branch
+    } else if (fgchild_pid == 0) {
+        
+        // add signal handlers
+        set_fgchild_sighandlers();
+        
+        // deal with redirects
+        if (curr_command->output_redirect != NULL) {
+            redirect_ouptut(curr_command->output_redirect);
+        }
+        if (curr_command->input_redirect != NULL) {
+        redirect_input(curr_command->input_redirect);
+        }
+              
+        // use execv to load and run new program
+        execv(curr_command->args[0], curr_command->args);
+
+        // if execv fails:
+        fprintf(stderr, "could not find command %s", curr_command->args[0]);
+        exit(1);
+
+    // parent branch
+    } else {
+        fgchild_pid = waitpid(fgchild_pid, &fgchild_status, 0);
+        if (WIFEXITED(fgchild_status)) {
+            last_fg_endmsg = LAST_FG_EXITED;
+            last_fg_endsig = WEXITSTATUS(fgchild_status);
+        } else {
+            last_fg_endmsg = LAST_FG_TERMINATED;
+            last_fg_endsig = WTERMSIG(fgchild_status);
+            last_fg_terminated = true;
+        }
+    }
+
+    return 1; // need this val because run_flag = 1 causes main while loop to repeat
+    // TO DO: consider making run_flag = 0 cause while loop to continue
+    // so that everything except for built-in exit can return 0 (more conventional)
+}
+
+// could have status call this function but would need to declare earlier
+void report_last_fg_end(void) {
+    printf("%s %d\n", last_fg_endmsg, last_fg_endsig);  
+}
+
+int launch_background(struct command* curr_command);
+
 
 int main(void) {
 
@@ -146,13 +242,15 @@ int main(void) {
     char expand_str[] = "$$";          //TO DO: Consider making this global and or a constant
 
     set_shell_sighandlers();
-
-    // TO DO: Set signal handlers
     
     while (run_flag) {
         
-        // TO DO: Need to check for and respond to completed bg processes
-        
+        // TODO: Check for and respond to completed bg processes
+        // TODO: Modify SIGSTP handler so that it only changes bool, then put msg function here
+        // TODO: Consider placing pointers to any pre-prompt messages in an array?? 
+        if (last_fg_terminated) {
+            report_last_fg_end();
+        }
         printf(C_PROMPT);
         fflush(stdout);  
         struct command *curr_command = get_command(expand_str, shell_pid_str);
@@ -169,9 +267,10 @@ int main(void) {
         } 
         // else if (bg_launch_allowed && curr_command->background) {
         //     run_flag = launch_background(curr_command);
-        // } else {
-        //     run_flag = launch_foreground(curr_command);
         // }
+        else {
+            run_flag = launch_foreground(curr_command);
+        }
      }
 
     return 0;
