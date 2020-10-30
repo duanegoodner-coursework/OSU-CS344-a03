bd2751d56b51ac1c2037c40f1d22f850a7c3ef80
Duane Goodner <dmgoodner@gmail.com>
2020-10-30 14:59:46 +0800

    add SIGINT and SIGTSTP handlers to shell

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -17,7 +19,6 @@
 #define LAST_FG_EXITED "The last foreground process terminated normally with exit status"
 
 
-
 struct bgpid_node
 {
     pid_t process_id;
@@ -26,25 +27,24 @@ struct bgpid_node
 
 struct bgpid_node *bg_list_head = NULL;
 struct bgpid_node *bg_list_tail = NULL;
+//struct bgpid_node* create_bg_pidnode(pid_t process_id);
 
 int last_fg_endsig = 0;
 char* last_fg_endmsg = NO_FG_RUN_YET;
+bool bg_launch_allowed = true;
+bool any_zombies = false;
 
-//struct bgpid_node* create_bg_pidnode(pid_t process_id);
+//int execute_command(struct command* curr_command);
+// int launch_foreground(struct command* curr_command);
+int launch_background(struct command* curr_command);
+int launch_foreground(struct command* curr_commane);
+
 
 int exit_bltin(struct command* curr_command);
 int cd_bltin(struct command* curr_command);
 int status_bltin(struct command* curr_command);
-
 int get_num_bltins(void);
 int get_bltin_index(struct command* curr_command);
-
-int execute_command(struct command* curr_command);
-// int launch_foreground(struct command* curr_command);
-//int launch_background(struct command* curr_command);
-
-
-
 
 char *bltin_funct_names[] = {
     "cd",
@@ -72,26 +72,6 @@ int get_bltin_index(struct command* curr_command) {
         }
     }
     return bltin_index;
-}
-
-int execute_command(struct command* curr_command) {
-    
-    
-    // Check if curr_command is a built-in function (and if it is, execute)
-    //TO DO: remove intermediate num_bltins and comparison vars after debug done
-    int num_bltins = get_num_bltins();
-    for (int index = 0; index < num_bltins; index++) {
-        int comparison = strcmp(curr_command->args[index], bltin_funct_names[index]);
-        if (comparison == 0) {
-            return (*bltin_funct_ptrs[index]) (curr_command);
-        }
-    }
-
-    // if (curr_command->background) {
-    //     return launch_background(curr_command);
-    // } else {
-    //     return launch_foreground(curr_command);
-    // }  
 }
 
 void killall_bgprocs(struct bgpid_node* bgpid_list) {
@@ -134,6 +114,30 @@ int cd_bltin(struct command* cd_command) {
     return 1;
 }
 
+#define ENTER_FG_ONLY_MSG "Entering foreground-only mode (& is now ignored)\n"
+#define EXIT_FG_ONLY_MSG "Exiting foreground-only mode\n"
+void handle_SIGTSTP (int signo) {
+    bg_launch_allowed = !bg_launch_allowed;
+    if (!bg_launch_allowed) {
+        write(STDOUT_FILENO, ENTER_FG_ONLY_MSG, 49);
+    } else {
+        write(STDOUT_FILENO, EXIT_FG_ONLY_MSG, 29);
+    }
+    write(STDOUT_FILENO, C_PROMPT, 2);       
+}
+
+void set_shell_sighandlers() {
+    struct sigaction ignore_action = {0};
+    struct sigaction SIGTSTP_action = {0};
+
+    ignore_action.sa_handler = SIG_IGN;
+    sigaction(SIGINT, &ignore_action, NULL);
+
+    SIGTSTP_action.sa_handler = handle_SIGTSTP;
+    SIGTSTP_action.sa_flags = SA_RESTART;
+    sigaction(SIGTSTP, &SIGTSTP_action, NULL);
+}
+
 int main(void) {
 
     int run_flag = 1;
@@ -141,6 +145,10 @@ int main(void) {
     char* shell_pid_str = int_to_dynstr(shell_pid);
     char expand_str[] = "$$";          //TO DO: Consider making this global and or a constant
 
+    set_shell_sighandlers();
+
+    // TO DO: Set signal handlers
+    
     while (run_flag) {
         
         // TO DO: Need to check for and respond to completed bg processes
@@ -154,11 +162,17 @@ int main(void) {
             continue;
         }
               
-        //Check if command is a "built-in" (and execut if it is)
+        //Check if command is a "built-in" (and execute if it is)
         int bltin_index = get_bltin_index(curr_command);
         if (bltin_index >= 0) {
             run_flag = (*bltin_funct_ptrs[bltin_index]) (curr_command);
-        }
+        } 
+        // else if (bg_launch_allowed && curr_command->background) {
+        //     run_flag = launch_background(curr_command);
+        // } else {
+        //     run_flag = launch_foreground(curr_command);
+        // }
      }
+
     return 0;
 }
